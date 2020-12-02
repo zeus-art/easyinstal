a98937b8b4d9d7f2c9ebc02ada6b8774b5d9a790
Zeus <73881701+zeus-art@users.noreply.github.com>
2020-12-02 11:52:32 +0100

    B1G update added steamcmd
    B1G update added steamcmd

---
+++ steam_cmd.sh
@@ -0,0 +1,6 @@
+sudo apt-get install lib32gcc1
+mkdir ~/Steam && cd ~/Steam
+curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+cd ~/Steam
+./steamcmd.sh
+login anonymous
